d18ce3adbdd5cb3a7e9ffa5641969e9855b10475
Niclas0923 <736012843@qq.com>
2022-04-16 11:33:41 +0800

    ie缓存问题解决

---
+++ 11AJAX/08/js.js
@@ -0,0 +1,40 @@
+//  引入express
+
+const { response } = require('express');
+const express = require('express');
+
+// 创建应用对象
+
+const app = express();
+
+// 创建路由规则
+// request是对请求报文的封装
+// response是对响应报文的封装
+
+app.get('/json-js',(request,response)=>{
+    // 设置响应头
+    response.setHeader('Access-Control-Allow-Origin','*')
+    let ss = {
+        name:"zhr",
+        age:19
+    }
+    let sss = JSON.stringify(ss)
+    // 设置响应体
+    response.send(sss)
+})
+
+app.get('/ie',(request,response)=>{
+    // 设置响应头
+    response.setHeader('Access-Control-Allow-Origin','*')
+
+    // 设置响应体
+    response.send('Hello IEd')
+})
+
+
+// 监听端口启动服务
+app.listen(8000,()=>{
+    console.log("服务已启动,8000端口监听ing")
+})
+
+// 通过nodemon运行js文件可以修改后直接自己重启服务器
