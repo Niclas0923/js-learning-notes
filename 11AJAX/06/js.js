54d2c95296f2e86e2cc2543ed5f85d5bf04e8ece
Niclas0923 <736012843@qq.com>
2022-04-16 10:12:17 +0800

    设置请求头

---
+++ 11AJAX/06/js.js
@@ -0,0 +1,44 @@
+//  引入express
+
+const { response } = require('express');
+const express = require('express');
+
+// 创建应用对象
+
+const app = express();
+
+// 创建路由规则
+// request是对请求报文的封装
+// response是对响应报文的封装
+// 可以接收任意类型的请求
+app.all('/js',(request,response)=>{
+    // 设置响应头
+    response.setHeader('Access-Control-Allow-Origin','*')
+    // 允许自定义响应头
+    response.setHeader('Access-Control-Allow-Headers','*')
+    // 设置响应体
+    response.send('hello post')
+})
+// 处理GET命令
+app.get('/js',(request,response)=>{
+    // 设置响应头
+    response.setHeader('Access-Control-Allow-Origin','*')
+    // 允许自定义响应头
+    response.setHeader('Access-Control-Allow-Headers','*')
+    // 设置响应体
+    response.send('hello')
+})
+// 处理POST命令
+app.post('/js',(request,response)=>{
+    // 设置响应头
+    response.setHeader('Access-Control-Allow-Origin','*')
+    // 允许自定义响应头
+    response.setHeader('Access-Control-Allow-Headers','*')
+    // 设置响应体
+    response.send('hello post')
+})
+
+// 监听端口启动服务
+app.listen(8000,()=>{
+    console.log("服务已启动,8000端口监听ing")
+})
