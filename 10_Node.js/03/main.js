cb49af627df7ef72202f4fe0800cf0259a34aba0
Niclas0923 <736012843@qq.com>
2022-04-23 13:47:57 +0800

    npm补充

--- 10_Node.js/03/main.js
+++ 10_Node.js/03/main.js
@@ -5,8 +5,13 @@ console.log(M.sum([123,456]))       // 579
 /*
     npm install 包名 --save
         安装包并添加到依赖
+    npm i 包名 -D
+        安装并添加到开发版中
     npm install
         根据依靠来下载包
     npm install --g
         全局安装(计算机中使用的工具，基本上都需要的那种)
+    npm init -y
+        快速生成package.json文件
+
 */
