aae8f892b9b8d13a0d701afbd722e0a48a5e4e95
Niclas0923 <736012843@qq.com>
2022-05-23 18:49:49 +0800

    声明，变量解构赋值，模板字符串

---
+++ 12_ES6-11/ES6/2_模板字符串.js
@@ -0,0 +1,17 @@
+/* 
+    ``
+    正常使用与单双引号相同
+*/
+const str = `123abc`
+/* 
+    可以在其中插入换行符
+    并且输出时也会换行
+*/
+console.log(`jjjjj
+jjjjj
+jjjjj`);
+
+const mm = "www"
+// 变量拼接
+const a =  `${123}是什么？${mm}`
+console.log(a)      // 123是什么？www
